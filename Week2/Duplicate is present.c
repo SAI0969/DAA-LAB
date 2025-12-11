a6ad21a742f372f823af88e97b2e3bf2b5b56f46
SAI0969 <asm1scs590526@gmail.com>
2025-12-11 15:16:41 +0530

    Add duplicate detection in array program
    This program checks for duplicate elements in an array entered by the user.

---
+++ Week2/Duplicate is present.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i, n, j, found = 0;
+    printf("Enter the number of elements in the array : ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    for(i = 0; i < n ; i++){
+        printf("Enter the elements %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    
+    for(i = 0; i < n - 1; i++){
+        for(j = i + 1; j < n; j++){
+            if(arr[i] == arr[j]){
+                found = 1;
+                break;
+            }
+           
+        }
+       if(found == 1)
+            break;
+    }
+    
+if(found == 1){
+    printf("Duplicate found");
+}
+else{
+    printf("Duplicate doesnot exists");
+}
+}
+
+
+example :- 
+
+Enter the number of elements in the array : 5
+Enter the elements 1: 15
+Enter the elements 2: 13
+Enter the elements 3: 19
+Enter the elements 4: 56
+Enter the elements 5: 15
+
+
+
+Output :- 
+
+Duplicate found
